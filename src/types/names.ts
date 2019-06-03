597ed695f54198156eb31625736b529b18ac616e
Denis Carriere <carriere.denis@gmail.com>
2019-06-03 12:45:48 -0700

    Add DAPPHDL

--- src/types/names.ts
+++ src/types/names.ts
@@ -8,3 +8,14 @@
  * names.DAPP // => "......2ke1.o4"
  */
 export const DAPP = "......2ke1.o4";
+
+/**
+ * DAPPHDL
+ *
+ * @example
+ *
+ * import { names } from "dapp-client"
+ *
+ * names.DAPPHDL // => ".1a4cm2ke1.o4"
+ */
+export const DAPPHDL = ".1a4cm2ke1.o4";
